afca9a1ccf4d8cdfd0ba0c29fb08afc78b1bebe2
RaguRamanTB <aakash1956ragu@gmail.com>
2020-03-21 11:20:08 +0530

    Start activity commenced

---
+++ app/src/main/java/android/music/sleep/activities/MainActivity.java
@@ -0,0 +1,110 @@
+package android.music.sleep.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.AlertDialog;
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.music.sleep.R;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+import android.os.Bundle;
+import android.view.View;
+import android.view.WindowManager;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    private CheckBox remember_me;
+    private TextView name, age;
+    private Button start;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        this.getWindow().setSoftInputMode(
+                WindowManager.LayoutParams.SOFT_INPUT_STATE_ALWAYS_HIDDEN);
+
+        initViews();
+
+        SharedPreferences sharedPreferences =
+                getSharedPreferences("checkbox", MODE_PRIVATE);
+        String r_check = sharedPreferences.getString("remember", "");
+
+        if (r_check.equals("true")) {
+            remember_me.setChecked(true);
+            name.setText(sharedPreferences.getString("name", ""));
+            age.setText(sharedPreferences.getString("age", ""));
+        }
+
+        if (!isConnected()) {
+            AlertDialog alertDialog;
+            AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
+            builder.setMessage("Please switch on your internet connection")
+                    .setCancelable(true);
+            alertDialog = builder.create();
+            alertDialog.setTitle("Connectivity Issue");
+            alertDialog.show();
+        }
+
+        remember_me.setOnCheckedChangeListener(
+                new CompoundButton.OnCheckedChangeListener() {
+                    @Override
+                    public void onCheckedChanged(CompoundButton buttonView,
+                                                 boolean isChecked) {
+                        if (buttonView.isChecked()) {
+                            SharedPreferences sharedPreferences =
+                                    getSharedPreferences("checkbox", MODE_PRIVATE);
+                            SharedPreferences.Editor editor = sharedPreferences.edit();
+                            editor.putString("remember", "true");
+                            editor.putString("name", name.getText().toString());
+                            editor.putString("age", age.getText().toString());
+                            editor.apply();
+                        } else if (!buttonView.isChecked()) {
+                            SharedPreferences sharedPreferences =
+                                    getSharedPreferences("checkbox", MODE_PRIVATE);
+                            SharedPreferences.Editor editor = sharedPreferences.edit();
+                            editor.putString("remember", "false");
+                            editor.apply();
+                        }
+                    }
+                });
+
+        start.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String getName = name.getText().toString();
+                String getAge = age.getText().toString();
+                if (getName.equals(" ") || getName.length() == 0
+                        || getAge.equals(" ") || getAge.length() == 0) {
+                    Toast.makeText(MainActivity.this, "Enter the credentials", Toast.LENGTH_LONG).show();
+                } else if (Integer.parseInt(getAge) > 150) {
+                    Toast.makeText(MainActivity.this, "Enter proper age value", Toast.LENGTH_LONG).show();
+                } else {
+                    startActivity(new Intent(MainActivity.this, StartActivity.class));
+                }
+            }
+        });
+    }
+
+    private void initViews() {
+        remember_me = findViewById(R.id.remember_me);
+        name = findViewById(R.id.name);
+        age = findViewById(R.id.age);
+        start = findViewById(R.id.startBtn);
+    }
+
+    public boolean isConnected() {
+        ConnectivityManager connectivityManager
+                = (ConnectivityManager) this.getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo activeNetworkInfo = connectivityManager.getActiveNetworkInfo();
+        return activeNetworkInfo != null && activeNetworkInfo.isConnected();
+    }
+}
--- app/src/main/java/android/music/sleep/activities/StartActivity.java
+++ app/src/main/java/android/music/sleep/activities/StartActivity.java
@@ -1,14 +1,15 @@
-package android.music.sleep;
+package android.music.sleep.activities;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.music.sleep.R;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class StartActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_start);
     }
 }
